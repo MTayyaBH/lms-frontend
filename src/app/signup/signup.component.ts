4e4af55991db7ace7a81f20474d783447b958cc9
Faizan Hamza <faizan8887r@gmail.com>
2024-08-27 14:27:23 +0500

    email varification

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 import { DbServiceService } from '../db-service.service'
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { Router } from '@angular/router';
+import * as CryptoJS from 'crypto-js';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -22,7 +23,9 @@ export class SignupComponent {
     password: new FormControl('', { validators: [Validators.required, Validators.minLength(4)], asyncValidators: [] }),
     confirmPassword: new FormControl('', { validators: [Validators.required], asyncValidators: [this.matchPassword()] })
   });
-
+  varifcationform = new FormGroup({
+    verification: new FormControl('', { validators: [Validators.required, Validators.minLength(5)], asyncValidators: [] }),
+  })
   matchPassword(): AsyncValidatorFn {
     return (control: AbstractControl): Observable<ValidationErrors | null> => {
       const password = this.signupForm.get('password');
@@ -32,7 +35,9 @@ export class SignupComponent {
       return of(null);
     };
   }
-
+  get verification(): AbstractControl {
+    return this.varifcationform.get('verification')!;
+  }
   get fullname(): AbstractControl {
     return this.signupForm.get('fullname')!;
   }
@@ -48,34 +53,105 @@ export class SignupComponent {
   get confirmPassword(): AbstractControl {
     return this.signupForm.get('confirmPassword')!;
   }
+  formsubmit: boolean = false
+  decrypt(encryptedMessage: string, key: string): string {
+    const bytes = CryptoJS.AES.decrypt(encryptedMessage, key);
+    return bytes.toString(CryptoJS.enc.Utf8);
+  }
+  signupnow() {
+    if (this.varifcationform.valid && this.signupForm.valid) {
+      let token = localStorage.getItem('token')
+      let parse = token ? JSON.parse(token) : null
+      let code=this.decrypt(parse.code,'verificationcode')
+      if (this.varifcationform.value.verification===code) {
+        this.startLoading();
+        this.http.post('signup-account', this.signupForm.value).subscribe({
+          next: (res: any) => {
+            if (res.error === 'Email already exists') {
+              this.createMessage('error', 'Email already exists');
+              this.formsubmit = false
+              this.closeLoading();
+            } else if (res.error) {
+              this.createMessage('error', res.error);
+              this.closeLoading();
+            } else {
+              this.createMessage('success', 'Account Created Successfully');
+              this.signupForm.reset()
+              this.varifcationform.reset()
+              localStorage.removeItem('token')
+              this.rout.navigate(['/Login'])
+            }
+          },
+          error: (err) => {
+            console.error(err);
+            this.createMessage('error', 'An error occurred');
+            this.closeLoading()
+          },
+          complete: () => {
+            this.closeLoading();
+          }
+        });
+      }else{
+        this.message.error('Invalid code!')
+      }
 
+    } else {
+      this.varifcationform.markAllAsTouched();
+      this.signupForm.markAllAsTouched();
+      this.message.error('An error occurred')
+    }
+  }
   submitForm() {
     if (this.signupForm.valid) {
-      this.startLoading();
-      this.http.post('signup-account', this.signupForm.value).subscribe({
-        next: (res: any) => {
-          if (res.error === 'Email already exists') {
-            this.createMessage('error', 'Email already exists');
-            this.closeLoading();
-          } else if (res.error) {
-            this.createMessage('error', res.error);
-            this.closeLoading();
-          } else {
-            this.createMessage('success', 'Account Created Successfully');
-            this.signupForm.reset()
-            this.rout.navigate(['/Login'])
-          }
-        },
-        error: (err) => {
-          console.error(err);
-          this.createMessage('error', 'An error occurred');
-          this.closeLoading()
-        },
-        complete: () => {
-          this.closeLoading();
+      try {
+        this.startLoading();
+        this.http.post('signup-account/email-varifcation', this.signupForm.value)
+          .subscribe(
+            (res: any) => {
+              console.log('Response:', res);
+              if (res) {
+                localStorage.setItem('token', JSON.stringify(res))
+                this.message.success('Email code sent to your email, please verify the code!')
+                this.formsubmit = true
+              }
+              this.closeLoading();
+            },
+            (error) => {
+              console.error('Error:', error);
+              this.closeLoading();
+              this.message.error('An error occurred')
+            }
+          );
+      } catch (error) {
+        console.log(error);
 
-        }
-      });
+      }
+
+      // this.startLoading();
+      // this.http.post('signup-account', this.signupForm.value).subscribe({
+      //   next: (res: any) => {
+      //     if (res.error === 'Email already exists') {
+      //       this.createMessage('error', 'Email already exists');
+      //       this.closeLoading();
+      //     } else if (res.error) {
+      //       this.createMessage('error', res.error);
+      //       this.closeLoading();
+      //     } else {
+      //       this.createMessage('success', 'Account Created Successfully');
+      //       this.signupForm.reset()
+      //       this.rout.navigate(['/Login'])
+      //     }
+      //   },
+      //   error: (err) => {
+      //     console.error(err);
+      //     this.createMessage('error', 'An error occurred');
+      //     this.closeLoading()
+      //   },
+      //   complete: () => {
+      //     this.closeLoading();
+
+      //   }
+      // });
     } else {
       this.signupForm.markAllAsTouched();
     }
